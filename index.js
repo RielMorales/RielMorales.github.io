02589ea660330406f4ef847d3c163b68354409a3
RielMorales <rmmorales1@up.edu.ph>
2024-03-01 11:49:04 +0800

    chore: update in validatePassword function: checker for password if it has numeric characters

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+const isNumeric = x => {
+  var i = 0;
+  var character = '';
+    while(i < x.length){
+      character = x.charAt(i);
+      if (!isNaN(character * 1)){
+        return true
+      }
+      i++
+    }
+  return false
+}
+
 // A function for validating if 2 passwords match.
 const validatePassword = (x, y) => {
   // checks if the two passwords match
   if (x === y) {
     if (x.length >= 8){
+      if (isNumeric(x)){
         return true
+      }
     }
   }
   return false //invalid password
