b42f313f2fa3940a38f4d11b8aa375878320beaf
RielMorales <rmmorales1@up.edu.ph>
2024-03-08 12:03:32 +0800

    chore: updated addAccount function in index.js: checks if inputs are valid, returns the needed output

--- exer4/index.js
+++ exer4/index.js
@@ -1,5 +1,6 @@
 //import of uuid package
 import { v4 as uuidv4 } from 'uuid';
+import validator from 'validator';
 
 // A function use to generate unique id based on first and last name.
 // it uses uuid package to generate a unqiue id
@@ -10,15 +11,30 @@ const generateUniqueID = (firstName, lastName) => {
     return "Last Name is Empty";
   } else {
     //creating unique id with a length of 8
-    const uniqueID = uuidv4().slice(0, 8)
+    const uniqueID = uuidv4().slice(0, 8);
     //returning the generated unique id
     return firstName.toLowerCase().slice(0,1) + lastName.toLowerCase() + uniqueID;
   }
 }
 
+function isNumber(value) {
+  return typeof value === 'number';
+}
+
 //A function use to add the details input by user to the list of accounts in a file named users.txt
-const addAccount = (firstName, lastName, email, age) => {
+const addAccount = ([firstName, lastName, email, age]) => {
+  const uniqueID = generateUniqueID(firstName, lastName);
+  if (uniqueID == "First Name is Empty" || uniqueID == "Last Name is Empty") return "First Name or Last Name input is empty";
+  if (validator.isEmail(email)){
+    if (isNumber(age)){
+      return firstName + ", " + lastName + ", " + email + ", " + age.toString() + ", " + uniqueID;
+    } else {
+      return age.toString() + " is not a valid input.\nInput must be a number."
+    }
+  } else {
+    return email + " is not a valid email"
+  }
 
 }
 
-export {generateUniqueID};
+export {generateUniqueID, addAccount};
--- exer4/test.js
+++ exer4/test.js
@@ -1,3 +1,4 @@
-import {generateUniqueID} from './index.js'
+import {generateUniqueID, addAccount} from './index.js'
 
 console.log(generateUniqueID("Riel", "Morales"));
+console.log(addAccount(["Riel", "Morales", "rmmorales1@up.edu.ph", 24]))
