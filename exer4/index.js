1e3165233fa2e4954a29584249e224c82f170c49
RielMorales <rmmorales1@up.edu.ph>
2024-03-08 11:45:40 +0800

    chore: updated generateUniqueID function in index.js: it can check if input is empty and return the unique id from the input name

--- exer4/index.js
+++ exer4/index.js
@@ -1,7 +1,19 @@
+//import of uuid package
+import { v4 as uuidv4 } from 'uuid';
+
 // A function use to generate unique id based on first and last name.
 // it uses uuid package to generate a unqiue id
 const generateUniqueID = (firstName, lastName) => {
-
+  //check if the first and last name is empty
+  if (firstName.length === 0) return "First Name is Empty";
+  if (lastName.length === 0) {
+    return "Last Name is Empty";
+  } else {
+    //creating unique id with a length of 8
+    const uniqueID = uuidv4().slice(0, 8)
+    //returning the generated unique id
+    return firstName.toLowerCase().slice(0,1) + lastName.toLowerCase() + uniqueID;
+  }
 }
 
-export {generateUniqueID}
+export {generateUniqueID};
--- exer4/test.js
+++ exer4/test.js
@@ -1,2 +1,3 @@
 import {generateUniqueID} from './index.js'
 
+console.log(generateUniqueID("Riel", "Morales"));
