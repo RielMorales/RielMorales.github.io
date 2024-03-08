569eda7fea1641cfcd0b44ed3b0cf1d026e6244e
RielMorales <rmmorales1@up.edu.ph>
2024-03-08 12:49:33 +0800

    chore: copied the exer directory

---
+++ exer4/test.js
@@ -0,0 +1,6 @@
+import {generateUniqueID, addAccount} from './index.js'
+
+// console.log(generateUniqueID("Alan", "Turing"));
+console.log(addAccount(["Alan", "Turing", "aturing@w3c.com", 58]));
+console.log(addAccount(["Tim", "Berners-Lee", "tim@w3c.com", 25]));
+console.log(addAccount(["Ted", "Nelson", "n@w3c.com", 43]));
