bb0845c2526d7e6a68d8a2ccd97e21f7c6b051c4
Riel Morales <rmmorales1@up.edu.ph>
2024-03-15 15:18:01 +0800

    chore: added GET Method for retrieving the entire book details with author as the only parameter

--- exer5/request.js
+++ exer5/request.js
@@ -6,6 +6,12 @@ needle.get(
     console.log(res.body);   // prints the body of the response message. In this case, “Hello”
 });
 
+needle.get(
+  'http://localhost:3000/find-by-isbn-author?author=J.K+Rowling', 
+  (err, res) => {
+    console.log(res.body);   // prints the body of the response message. In this case, “Hello”
+});
+
 needle.post(    
   'http://localhost:3000/add-book',
   { bookName: 'Harry Potter and the Philosopher\'s Stone',
