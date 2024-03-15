4b3252e5b70ac76b2a432049c8abc650a494b840
RielMorales <rmmorales1@up.edu.ph>
2024-03-15 12:10:37 +0800

    chore: updated server.js file

---
+++ exer5/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+// instantiate the server
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// this tells our app to listen for GET messages on the '/' path
+// the callback function specifies what the server will do when a message is received
+app.get('/', (req, res) => {
+  res.send('Hello!');
+});
+
+// this tells our server to listen to the port 3000
+// we can also pass an optional callback function to execute after the server starts
+app.listen(3000, () => { console.log('Server started at port 3000')} );
+
