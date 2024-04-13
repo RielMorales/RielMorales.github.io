d57a6880aa248be6db655c778049520cc1fd9139
Riel Morales <rmmorales1@up.edu.ph>
2024-04-13 23:31:18 +0800

    chore: added type module in package.json; updated event-script.js and form.js file

--- exer7/static_files/event-script.js
+++ exer7/static_files/event-script.js
@@ -1,14 +1,11 @@
 const submitButton = document.getElementById("submit-button")
 const addFood = (req, res) => {
-  // alert('Adding Favorite Food')
+  alert('Adding Favorite Food')
   // if(req.body.foodname.length != 0){
   //   console.log(req.body.foodname)
   // }
-  console.log(req.body)
+//   console.log(req.body)
 }
 
-if(submitButton.getAttribute("foodname") == ""){
-    console.log("Empty Food Name")
-} else {
-    submitButton.addEventListener("click", addFood)
-}
+submitButton.addEventListener("click", addFood)
+
