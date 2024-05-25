006bc3694f6bf16701f45994a01ed1285961506c
Riel Morales <rmmorales1@up.edu.ph>
2024-05-25 22:53:35 +0800

    chore: Added Screenshots of Results

--- exer6/request.js
+++ exer6/request.js
@@ -1,71 +1,71 @@
 import needle from 'needle';
 
 
-// //add student
-// needle.post(
-//     'http://localhost:3000/save-student',
-//     {
-//         stdnum: "2024-00001",
-//         fname: "Mary Jane",
-//         lname: "Watson",
-//         age: 18
-//     },
-//     (err, res) => {
-//         console.log(res.body);
-//     }
-// );
+//add student
+needle.post(
+    'http://localhost:3000/save-student',
+    {
+        stdnum: "2024-00001",
+        fname: "Mary Jane",
+        lname: "Watson",
+        age: 18
+    },
+    (err, res) => {
+        console.log(res.body);
+    }
+);
 
-// needle.post(
-//     'http://localhost:3000/save-student',
-//     {
-//         stdnum: "2024-00002",
-//         fname: "Norman",
-//         lname: "Osborn",
-//         age: 19
-//     },
-//     (err, res) => {
-//         console.log(res.body);
-//     }
-// );
+needle.post(
+    'http://localhost:3000/save-student',
+    {
+        stdnum: "2024-00002",
+        fname: "Norman",
+        lname: "Osborn",
+        age: 19
+    },
+    (err, res) => {
+        console.log(res.body);
+    }
+);
 
-// needle.post(
-//     'http://localhost:3000/save-student',
-//     {
-//         stdnum: "2024-00003",
-//         fname: "May",
-//         lname: "Parker",
-//         age: 20
-//     },
-//     (err, res) => {
-//         console.log(res.body);
-//     }
-// );
+needle.post(
+    'http://localhost:3000/save-student',
+    {
+        stdnum: "2024-00003",
+        fname: "May",
+        lname: "Parker",
+        age: 20
+    },
+    (err, res) => {
+        console.log(res.body);
+    }
+);
 
-// needle.post(
-//     'http://localhost:3000/save-student',
-//     {
-//         stdnum: "2024-00004",
-//         fname: "Curt",
-//         lname: "Connors",
-//         age: 19
-//     },
-//     (err, res) => {
-//         console.log(res.body);
-//     }
-// );
+needle.post(
+    'http://localhost:3000/save-student',
+    {
+        stdnum: "2024-00004",
+        fname: "Curt",
+        lname: "Connors",
+        age: 19
+    },
+    (err, res) => {
+        console.log(res.body);
+    }
+);
 
-// needle.post(
-//     'http://localhost:3000/save-student',
-//     {
-//         stdnum: "8051495845",
-//         fname: "Peter",
-//         lname: "Parker",
-//         age: 21
-//     },
-//     (err, res) => {
-//         console.log(res.body);
-//     }
-// );
+needle.post(
+    'http://localhost:3000/save-student',
+    {
+        stdnum: "8051495845",
+        fname: "Peter",
+        lname: "Parker",
+        age: 21
+    },
+    (err, res) => {
+        console.log(res.body);
+    }
+);
 
 
 // //update student
@@ -99,16 +99,16 @@ import needle from 'needle';
 //     }
 // );
 
-//search student
-needle.get(
-    'http://localhost:3000/user?stdnum=8051495845', 
-    (err, res) => {
-    console.log(res.body);
-});
+// //search student
+// needle.get(
+//     'http://localhost:3000/user?stdnum=8051495845', 
+//     (err, res) => {
+//     console.log(res.body);
+// });
 
-//show all student
-needle.get(
-    'http://localhost:3000/members', 
-    (err, res) => {
-    console.log(res.body);
-});
+// //show all student
+// needle.get(
+//     'http://localhost:3000/members', 
+//     (err, res) => {
+//     console.log(res.body);
+// });
