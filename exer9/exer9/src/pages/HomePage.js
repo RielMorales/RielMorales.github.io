3958b5247de1c8fe17194a0453557c8da12ef311
Riel Morales <rmmorales1@up.edu.ph>
2024-05-28 02:08:45 +0800

    fix: fixed the total products of added product in cart

--- exer9/exer9/src/pages/HomePage.js
+++ exer9/exer9/src/pages/HomePage.js
@@ -6,29 +6,37 @@ export default function HomePage(props) {
     // console.log(props.menuData[2])
     let navMenus = props.menuData;
     let products = navMenus[1];
+    let productTotal = 0;
+    let i = 0;
+    while (i < props.menuData[3].length){
+        productTotal = productTotal + props.menuData[3][i].qty
+        i = i + 1;
+    }
     return (
         <div className="page">
             <PageHeader data={navMenus[0]} />
             <div className="HomePageBody">
-                <div className="products">
-                    {
-                        products.map((product) => {
-                            return (
-                                <Product key={product.id} data={[product, props.menuData[2], props.menuData[3]]} />
-                            )
-                        })
-                    }
+                <div className="products-container">
+                    <div className="products">
+                        {
+                            products.map((product) => {
+                                return (
+                                    <Product key={product.id} data={[product, props.menuData[2], props.menuData[3]]} />
+                                )
+                            })
+                        }
+                    </div>
                 </div>
 
                 <div className="cart-container">
                     <div className="cart">
-                        <h3>Shopping Cart: {props.menuData[3].length}</h3>
+                        <h3>Shopping Cart: {productTotal}</h3>
                         
                         <div className="cart-list">
                             {
                                 (() => {
                                     if (props.menuData[3].length == 0){
-                                        return <h3>There is no product in cart.</h3>
+                                        return <h3 className="no-products">There is no product in cart.</h3>
                                     } else {
                                         return (
                                             props.menuData[3].map((cart, index) => {
